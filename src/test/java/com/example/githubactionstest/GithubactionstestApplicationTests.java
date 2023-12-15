567ecd0ce44af49d0f749668a8c7b9d97db2fabc
HYESOO KIM <khwater1017@naver.com>
2023-12-15 14:36:17 +0900

    깃허브 액션 테스트

---
+++ src/test/java/com/example/githubactionstest/GithubactionstestApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.githubactionstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void testFail(){
+		throw new RuntimeException("실패했다");
+
+	}
+
+}
